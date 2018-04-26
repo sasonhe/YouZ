4271550cd99bf60049c26f511ab9d15ce13daab8
sasonhe <642139009@qq.com>
2018-04-26 12:55:26 +0800

    add swiper

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,11 +1,6 @@
 <template>
 <div class="banner">
-  <!-- <ul>
-    <li><a href="javascript:;"><img src="../assets/images/banner.jpg" alt=""></a></li>
-  </ul> -->
-  <slider :pages="pages" :sliderinit="sliderinit">
-    <!-- slot  -->
-  </slider>
+  <slider :pages="pages" :sliderinit="sliderinit"></slider>
 </div>
 </template>
 
@@ -16,34 +11,33 @@ export default {
     return {
       //图片列表[arr]
       pages: [{
-          title: '',
+          html: `<img src='${require('../../static/images/banner.jpg')}'>`,
           style: {
-            background: `url(${require('../../static/images/banner.jpg')})`
+            background: '#e7e7e7'
           }
         },
         {
-          title: '',
+          html: `<img src='${require('../../static/images/banner.jpg')}'>`,
           style: {
-            background: `url(${require('../../static/images/banner.jpg')})`
+            background: '#333'
           }
         },
         {
-          title: 'slide3',
+          html: `<img src='${require('../../static/images/banner.jpg')}'>`,
           style: {
-            background: `url(${require('../../static/images/banner.jpg')})`
+            background: '#ccc'
           },
         }
       ],
       //滑动配置[obj]
       sliderinit: {
-        currentPage: 0, //当前页码
-        thresholdDistance: 500, //滑动判定距离
-        thresholdTime: 100, //滑动判定时间
-        autoplay: 3000, //自动滚动[ms]
-        loop: true, //循环滚动
-        direction: 'horizontal', //方向设置，垂直滚动
-        infinite: 1, //无限滚动前后遍历数
-        slidesToScroll: 1, //每次滑动项数
+        currentPage: 0,
+        thresholdDistance: 100,
+        thresholdTime: 300,
+        loop: true,
+        infinite: 1,
+        autoplay: 3000,
+        slidesToScroll: 1
       }
     }
   },
